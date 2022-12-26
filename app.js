f2f434336abae6fb25c82a23e3e6c415949ed5b6
jaydeep2301 <110085656+jaydeep2301@users.noreply.github.com>
2022-12-26 17:02:29 +0530

    express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT=process.env.PORT||3000;
+app.listen(PORT,async ()=>{
+    console.log(`server started on ${PORT}`);
+
+})
+app.get("/",async (req,res)=>{
+    return res.status(200).send("hello world")
+})
